d12b3e1cd0c2d4b948ff18834417272744be36c3
luning95 <luning95@126.com>
2018-11-27 12:19:19 +0800

    First project of Git

--- src/main/java/com/ldn/sales/SMApplication.java
+++ src/main/java/com/ldn/sales/SMApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+
 @SpringBootApplication
+/**
+ *可以缓存
+ */
 @EnableCaching
 public class SMApplication {
 
